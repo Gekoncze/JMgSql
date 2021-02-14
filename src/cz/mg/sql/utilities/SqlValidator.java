77748de684106374f0ba07e9aea55511322d6c74
Me <gekoncze@seznam.cz>
2021-02-14 16:33:02 +0100

    added validation method for names

--- src/cz/mg/sql/utilities/SqlValidator.java
+++ src/cz/mg/sql/utilities/SqlValidator.java
@@ -26,4 +26,45 @@ public class SqlValidator {
         }
         return count;
     }
+
+    public static void validateName(String name){
+        for(int i = 0; i < name.length(); i++){
+            char ch = name.charAt(i);
+            if(!isAllowedNameCharacter(ch, i == 0)){
+                throw new IllegalArgumentException("Illegal character '" + ch + "' in name '" + name + "'.");
+            }
+        }
+    }
+
+    private static boolean isAllowedNameCharacter(char ch, boolean begin){
+        if(begin){
+            return isAllowedAlphabetic(ch);
+        } else {
+            return isAllowedAlphanumeric(ch) || isAllowedSpecial(ch);
+        }
+    }
+
+    private static boolean isAllowedAlphanumeric(char ch){
+        return isAllowedAlphabetic(ch) || isAllowedNumber(ch);
+    }
+
+    private static boolean isAllowedAlphabetic(char ch){
+        return isAllowedUpperCase(ch) || isAllowedLowerCase(ch);
+    }
+
+    private static boolean isAllowedNumber(char ch){
+        return ch >= '0' && ch <= '9';
+    }
+
+    private static boolean isAllowedUpperCase(char ch){
+        return ch >= 'A' && ch <= 'Z';
+    }
+
+    private static boolean isAllowedLowerCase(char ch){
+        return ch >= 'a' && ch <= 'z';
+    }
+
+    private static boolean isAllowedSpecial(char ch){
+        return ch == '_';
+    }
 }
